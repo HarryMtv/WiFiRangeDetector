ae5f05fe96f47af76e05eb0ec4437f392c7b390e
igor.martynov <igor.martynov@team.wrike.com>
2018-03-19 21:09:14 +0300

    Add menu

---
+++ app/src/main/java/com/martynov/igor/detectwifiarea/ConnectFragment.java
@@ -0,0 +1,26 @@
+package com.martynov.igor.detectwifiarea;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class ConnectFragment extends Fragment {
+
+    public ConnectFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_connect, container, false);
+
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/martynov/igor/detectwifiarea/TableFragment.java
@@ -0,0 +1,21 @@
+package com.martynov.igor.detectwifiarea;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TableFragment extends Fragment {
+
+    public TableFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_table, container, false);
+
+        return rootView;
+    }
+
+}
